97f99f209380e55a1ad639e25c7363a754d32755
yuvraj <yuvrajsingh532004@gmail.com>
2024-10-11 00:22:39 +0530

    Home route also added in test

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express')
 const port = 9090
 const app = express()
 
+app.get('/',(req,res)=>{
+    res.status(200).json({success:true , message:'Home route'})
+})
+
 app.get('/test-route',(req,res)=>{
     res.status(200).json({success:true , message:'testing route works fine'})
 })
